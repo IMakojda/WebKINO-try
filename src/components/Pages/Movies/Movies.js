829e63dd7d37acf0b4b200a8d9592e085bf3e91a
Igor Makojda <86243683+IMakojda@users.noreply.github.com>
2022-03-13 10:43:25 +0200

    movie start

--- src/components/Pages/Movies/Movies.js
+++ src/components/Pages/Movies/Movies.js
@@ -1,10 +1,25 @@
-const Movies = ()=>{
+// import axios from "axios";
+// import { useState ,useEffect} from "react";
 
-  return(
-    <div>
-      <span className="pageTitle">Movies</span>
-    </div>
-  )
-}
 
-export default Movies;
+
+// const Movies = ()=>{
+//   const [page, setPage] = useState(1);
+//   const [content, setContent] = useState([]);
+ 
+  
+  
+//   const fetchMovies = async ()=> {
+//     const {data} = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`) 
+//     setContent(data.results);
+//     }
+
+
+//   return(
+//     <div>
+//       <span className="pageTitle">Movies</span>
+//     </div>
+//   )
+// }
+
+// export default Movies;
